85370382e8bcf13edaeb8fd04a7ec14872d0e404
downkhg <downkhg@gmail.com>
2021-01-28 21:01:42 +0900

    컴퓨터 그래픽스 계층구조,터레인,라이트,렌더링파이프라인,메테리얼,빌보드이펙트

---
+++ CompterGraphics/Assets/Scipts/BillBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    public Camera m_cCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UV: 텍스쳐 좌표
+        //빌보드: 카메라를 바라보는 평면
+        //텍스쳐: 보여주는 이미지
+        //행렬: 회전행렬은 11~33까지의 원소가 회전행렬이다.
+        this.transform.LookAt(m_cCamera.transform);
+
+        Renderer renderer = GetComponent<Renderer>();
+        Material material = renderer.material;
+
+        material.mainTextureOffset = new Vector2(0, 0);
+        material.mainTextureScale = new Vector2(1, 1);
+    }
+}
---
+++ CompterGraphics/Assets/Scipts/ReflectTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectTest : MonoBehaviour
+{
+    public GameObject objReflect; //반사될 물체
+    public float fSpeed;
+    public Vector3 vDir; //반사될 물체의 방향
+    public Plane cPlane; //반사될 평면
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Vector3 vA = new Vector3(0, 0, 0);
+        Vector3 vB = new Vector3(1, 0, 0);
+        Vector3 vC = new Vector3(0, 0, 1);
+
+        Vector3 vReflectPos = objReflect.transform.position;
+        Vector3 vPlanePos = cPlane.normal * cPlane.distance;
+        vDir = vPlanePos - vReflectPos; //공이 있는 위치에서 평면 위치로 입사각을 계산한다.
+        vDir.Normalize();
+        cPlane = new Plane(vA,vC,vB);
+    }
+    public float fCosT;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vRelfectPos = objReflect.transform.position;
+
+        vRelfectPos += vDir * fSpeed * Time.deltaTime;
+
+        Vector3 vPlanePos = cPlane.normal * cPlane.distance;
+        Vector3 vDist = vRelfectPos - vPlanePos;
+        //vDir.Normalize();
+        Vector3 vPlaneNormal = cPlane.normal;
+        Debug.DrawLine(vRelfectPos, vRelfectPos + (vDir *2),Color.red);
+        fCosT = Vector3.Dot(vPlaneNormal, vDist.normalized);
+        if (fCosT <= 0)
+        {
+            //Debug.LogError("reflect");
+            vDir = Vector3.Reflect(vDir, cPlane.normal);
+        }
+
+        objReflect.transform.position = vRelfectPos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 vStart = cPlane.normal * cPlane.distance;
+        Vector3 vEnd = vStart + cPlane.normal * 1;
+
+        Gizmos.DrawLine(vStart, vEnd);
+    }
+}
