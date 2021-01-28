197e3460aa22d16f44be50a1be4884ffbcbee5ee
downkhg <downkhg@gmail.com>
2021-01-28 13:42:32 +0900

    문열기 테스트

---
+++ RoomExitUnity/Assets/Scprits/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public DoorAsix m_cDoorAsix;
+
+    public void Open()
+    {
+        m_cDoorAsix.Open();
+    }
+
+    private void OnMouseDown()
+    {
+        m_cDoorAsix.Open();
+    }
+}
---
+++ RoomExitUnity/Assets/Scprits/Dynamic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    public float m_fSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * m_fSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * m_fSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * 1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down * 1);
+        }
+    }
+}
